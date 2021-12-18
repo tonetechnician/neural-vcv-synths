af233c2ab117773b4d37df4b6d389d535dbd1b2d
Sean <tonetechnician@gmail.com>
2021-12-18 21:31:47 +0200

    feat: add start to ddsp library loader

--- cpp/Nash-Suite/src/ddsp.cpp
+++ cpp/Nash-Suite/src/ddsp.cpp
@@ -1,5 +1,10 @@
 #include "plugin.hpp"
+#include <osdialog.h>
+#include <stdlib.h>
+#include "ddsp_model.h"
+#include <audio.hpp>
 
+static const size_t BLOCK_SIZE = 4096;
 
 struct Ddsp : Module {
 	enum ParamId {
@@ -18,15 +23,37 @@ struct Ddsp : Module {
 		LIGHTS_LEN
 	};
 
+	DDSPModel* ddspModel;
+
 	Ddsp() {
 		config(PARAMS_LEN, INPUTS_LEN, OUTPUTS_LEN, LIGHTS_LEN);
-		configInput(PITCH_INPUT, "");
-		configInput(LOUDNESS_INPUT, "");
-		configOutput(OUTPUT_OUTPUT, "");
+		configInput(PITCH_INPUT, "Pitch");
+		configInput(LOUDNESS_INPUT, "Loudness");
+		configOutput(OUTPUT_OUTPUT, "Audio");
+		std::cout  << "Hi from ddsp" << std::endl;
+		ddspModel = new DDSPModel();
 	}
 
 	void process(const ProcessArgs& args) override {
+		float out = 0;
+		if (inputs[PITCH_INPUT].isConnected())
+		{
+			float pitch = inputs[PITCH_INPUT].getVoltage();
+			float loudness = inputs[LOUDNESS_INPUT].getVoltage();
+			ddspModel->perform(&pitch, &loudness, &out, 4096);
+			outputs[OUTPUT_OUTPUT].setVoltage(rack::math::clamp(out, -5.0f, 5.0f));
+		}
 	}
+
+	void openDialogAndLoadModel() {
+		filepath = osdialog_file(osdialog_file_action::OSDIALOG_OPEN, nullptr, nullptr, nullptr);
+		if (filepath)
+		{
+			ddspModel->load(at::str(filepath));
+			std::cout  << "Model loaded" << std::endl;
+		}
+	}
+	char* filepath;
 };
 
 
@@ -40,10 +67,19 @@ struct DdspWidget : ModuleWidget {
 		addChild(createWidget<ScrewSilver>(Vec(RACK_GRID_WIDTH, RACK_GRID_HEIGHT - RACK_GRID_WIDTH)));
 		addChild(createWidget<ScrewSilver>(Vec(box.size.x - 2 * RACK_GRID_WIDTH, RACK_GRID_HEIGHT - RACK_GRID_WIDTH)));
 
-		addInput(createInputCentered<PJ301MPort>(mm2px(Vec(6.809, 10.576)), module, Ddsp::PITCH_INPUT));
-		addInput(createInputCentered<PJ301MPort>(mm2px(Vec(22.311, 10.431)), module, Ddsp::LOUDNESS_INPUT));
+		addInput(createInputCentered<PJ301MPort>(mm2px(Vec(7.489, 10.576)), module, Ddsp::PITCH_INPUT));
+		addInput(createInputCentered<PJ301MPort>(mm2px(Vec(22.706, 10.431)), module, Ddsp::LOUDNESS_INPUT));
 
-		addOutput(createOutputCentered<PJ301MPort>(mm2px(Vec(15.067, 113.581)), module, Ddsp::OUTPUT_OUTPUT));
+		addOutput(createOutputCentered<PJ301MPort>(mm2px(Vec(15.24, 113.581)), module, Ddsp::OUTPUT_OUTPUT));
+	}
+
+	void appendContextMenu(Menu* menu) override {
+		Ddsp* module = dynamic_cast<Ddsp*>(this->module);
+		assert(module);
+
+		menu->addChild(new MenuSeparator);
+
+		menu->addChild(createMenuItem("Load Module", "", [=]() {module->openDialogAndLoadModel();}));
 	}
 };
 
