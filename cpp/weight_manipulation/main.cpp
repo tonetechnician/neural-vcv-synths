784eb2b6acc10e8a98516e4e5f1b40591eb80ae1
Franco Caspe <francocaspe@gmail.com>
2021-12-19 10:16:53 +0000

    Tested weight interpolation.

--- cpp/weight_manipulation/main.cpp
+++ cpp/weight_manipulation/main.cpp
@@ -12,6 +12,9 @@ int main(int argc, const char* argv[])
     using std::chrono::duration;
     using std::chrono::milliseconds;
 
+    /*Disable grad computation - it will allow change weights one by one.*/
+    torch::NoGradGuard no_guard; //Will only disable grads in current thread.
+
     if (argc != 2)
     {
     std::cerr << "usage: example-app <path-to-exported-script-module>\n";
@@ -32,20 +35,37 @@ int main(int argc, const char* argv[])
         }
 
     std::cout << "module loaded.\n";
+    
     auto t1 = high_resolution_clock::now();
     
-    std::vector<torch::Tensor> test_vct;
-    copy_params_to_vector(module,test_vct);
-    print_weights_vector(test_vct);
-
-    reset_weights(module);
+    std::vector<torch::Tensor> state1,state2;
+    /*Copy model parameters to vector.*/
+    copy_params_to_vector(module,state1);
+    /*Set all model weights to zero.*/
+    reset_weights(module);    
+    /*Copy all zeros to vector*/
+    copy_params_to_vector(module,state2);
+    
+    /*
+    std::cout << "STATE 1 \n========" << std::endl;
+    print_weights_vector(state1);
+    std::cout << "STATE 2 \n========" << std::endl;
+    print_weights_vector(state2);
+    */
+    
+    
+    /*state 1 has original module weights, - state 2 has all zeros. 
+      The model should have all its original weights halved. */
+    interpolate_weights(module,state1,state2,0.5);
+    
+    /*std::cout << "INTERPOLATED\n========" << std::endl;
+    print_weights(module);
+    */
     
     auto t2 = high_resolution_clock::now();
-
-    auto ms_int = duration_cast<milliseconds>(t2 - t1);    
-
+    auto ms_int = duration_cast<milliseconds>(t2 - t1);
+    
     std::cout << "done in " << ms_int.count() << "ms\n";
-    print_weights(module);
-
+    
     return 0;
 }
