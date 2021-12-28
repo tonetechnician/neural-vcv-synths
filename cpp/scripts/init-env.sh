7cbca6907dae2e35a6abe5559bcf786b45749a9d
Sean Devonport <tonetechnician@gmail.com>
2021-12-28 20:16:26 +0200

    feat: add basic helper makefile scripts for easy setup

---
+++ cpp/scripts/init-env.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+echo "Initializing environment"
+
+LIBTORCH_VER="1.10.1"
+
+if [[ "$OSTYPE" == "darwin"* ]] ; then
+  echo "Set up darwin"
+  echo "Downloading libtorch shared libraries"
+  wget https://download.pytorch.org/libtorch/cpu/libtorch-macos-$LIBTORCH_VER.zip -P ./tmp
+  unzip ./tmp/libtorch-macos-$LIBTORCH_VER.zip -d ./Nash-Suite/dep/libtorch
+elif [[ "$OSTYPE" == "linux-gnu"* ]] ; then
+  echo "Set up linux"
+  echo "Downloading libtorch shared librariess with cxx11 ABI"
+  wget https://download.pytorch.org/libtorch/cpu/libtorch-cxx11-abi-shared-with-deps-$LIBTORCH_VER%2Bcpu.zip -P ./tmp
+  unzip ./tmp/libtorch-macos-$LIBTORCH_VER.zip -d ./Nash-Suite/dep/libtorch
+fi
