dfd3c942dbbad8311a205ec562738bba2796b864
Sean Devonport <tonetechnician@gmail.com>
2021-12-29 13:11:12 +0200

    fix: lowercase .svg being referenced, should be uppercase

--- cpp/Nash-Suite/src/ddsp.cpp
+++ cpp/Nash-Suite/src/ddsp.cpp
@@ -154,7 +154,7 @@ struct Ddsp : Module {
 struct DdspWidget : ModuleWidget {
 	DdspWidget(Ddsp* module) {
 		setModule(module);
-		setPanel(createPanel(asset::plugin(pluginInstance, "res/ddsp.svg")));
+		setPanel(createPanel(asset::plugin(pluginInstance, "res/DDSP.svg")));
 
 		addChild(createWidget<ScrewSilver>(Vec(RACK_GRID_WIDTH, 0)));
 		addChild(createWidget<ScrewSilver>(Vec(box.size.x - 2 * RACK_GRID_WIDTH, 0)));
